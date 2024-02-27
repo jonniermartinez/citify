598279aba8e5f9ff6c862abcaaae67a7d9a36e93
jonnier <jonnierosjitas@gmail.com>
2024-02-27 18:07:50 -0500

    fix

--- src/app/[locale]/(routes)/result/page.tsx
+++ src/app/[locale]/(routes)/result/page.tsx
@@ -1,11 +1,9 @@
 import MainResult from "./components/main";
 import { useTranslations } from "next-intl";
-import { useRouter } from "next/navigation";
 import useCitiation from "@/hooks/citiation-state";
 
 function Result() {
   const t = useTranslations("result");
-  const router = useRouter();
   const citiation = useCitiation();
 
   if (!citiation.url) {
