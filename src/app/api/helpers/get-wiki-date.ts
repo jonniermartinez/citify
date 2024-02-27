22409f229177ce86c03cdec88213d612a0844fe3
jonnier <jonnierosjitas@gmail.com>
2024-02-27 18:45:38 -0500

    puputier·

--- src/app/api/helpers/get-wiki-date.ts
+++ src/app/api/helpers/get-wiki-date.ts
@@ -1,10 +1,15 @@
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-core";
+import chromium from "@sparticuz/chromium-min";
 
 export async function obtenerFechaEdicion(url: string) {
+  // Or use
+  const chromiumPack =
+    "https://github.com/Sparticuz/chromium/releases/download/v121.0.0/chromium-v121.0.0-pack.tar";
   const browser = await puppeteer.launch({
-    headless: false,
-    defaultViewport: null,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    args: chromium.args,
+    // See https://www.npmjs.com/package/@sparticuz/chromium#running-locally--headlessheadful-mode for local executable path
+    executablePath: await chromium.executablePath(chromiumPack),
+    headless: true,
   });
   const page = await browser.newPage();
   await page.goto(url, {
