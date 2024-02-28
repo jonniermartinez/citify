c7376467913694283decc8bc42e0bf9c2ec4ba1c
jonnier <jonnierosjitas@gmail.com>
2024-02-27 20:58:10 -0500

    fix wikipedia case

--- src/app/api/helpers/get-wiki-date.ts
+++ src/app/api/helpers/get-wiki-date.ts
@@ -1,3 +1,23 @@
+import { JSDOM } from "jsdom";
+
 export async function obtenerFechaEdicion(url: string) {
-  return "HOLA mundo";
+  try {
+    const res = await fetch(url);
+    const html = await res.text();
+
+    const dom = new JSDOM(html);
+
+    // Assuming there's only one element with the class "footer-info-lastmod"
+    const lastModifiedDateElement = dom.window.document.querySelector(
+      ".mw-footer-container"
+    );
+
+    if (lastModifiedDateElement) {
+      const lastEdit = lastModifiedDateElement.textContent;
+      console.log(lastEdit);
+      return lastEdit;
+    }
+  } catch (error) {
+    return "Error fetching or parsing the HTML:";
+  }
 }
