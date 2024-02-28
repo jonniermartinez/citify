06fa614d535467afec8c7f28768721aa2fe823fb
jonnier <jonnierosjitas@gmail.com>
2024-02-27 20:20:49 -0500

    fix error date wikipedia

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,10 @@
 import createNextIntlPlugin from 'next-intl/plugin';
-/** @type {import('next').NextConfig} */
-
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverComponentsExternalPackages: ["puppeteer-core"],
-      },
+    
 };
 
 export default withNextIntl(nextConfig);
--- src/app/api/helpers/get-wiki-date.ts
+++ src/app/api/helpers/get-wiki-date.ts
@@ -1,29 +1,3 @@
-import puppeteer from "puppeteer-core";
-import chromium from "@sparticuz/chromium-min";
-
 export async function obtenerFechaEdicion(url: string) {
-  // Or use
-  const chromiumPack =
-    "https://github.com/Sparticuz/chromium/releases/download/v121.0.0/chromium-v121.0.0-pack.tar";
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    // See https://www.npmjs.com/package/@sparticuz/chromium#running-locally--headlessheadful-mode for local executable path
-    executablePath: await chromium.executablePath(chromiumPack),
-    headless: true,
-  });
-  const page = await browser.newPage();
-  await page.goto(url, {
-    waitUntil: "domcontentloaded",
-  });
-
-  await page.waitForSelector("#footer-info-lastmod");
-  const info = await page.evaluate(() => {
-    const data = document.getElementById("footer-info-lastmod");
-    return data?.innerHTML;
-  });
-  console.log(info);
-
-  // Cerrar el navegador
-  await browser.close();
-  return info;
+  return "HOLA mundo";
 }
